efe57b04bde269ff5ae6b0e04b6cd4273b83c862
Sam Mustapha <W0843850@myscc.ca>
2024-10-04 02:27:37 -0400

    finished

--- Sam_Mustapha_Learning_Angular_/src/app/car-list/car-list.component.ts
+++ Sam_Mustapha_Learning_Angular_/src/app/car-list/car-list.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cars } from '../Shared/Model/cars';
 import {NgClass, NgForOf} from '@angular/common';
 import { CarListItemComponent } from '../car-list-item/car-list-item.component';
-import { cars } from '../Shared/Model/mockCar';
+import { CarService } from '../services/car.service';
 
 
 @Component({
@@ -12,8 +12,27 @@ import { cars } from '../Shared/Model/mockCar';
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.scss'
 })
-export class CarListComponent {
+export class CarListComponent implements OnInit{
+  dispaly: String [] = ['id', 'name', 'price', 'model'];
+  cars: Cars[] =[];
+  constructor(private carService: CarService){
+
+  }
+
+  ngOnInit() {
+      this.carService.getCars().subscribe({
+        next: (data: Cars[])=> this.cars = data,
+        error:err => console.error("error finding car", err),
+        complete:() => console.log("finding conmpleted")
+      })
+  }
+
+  selectedCarItem?: Cars;
+  selectedCar(car : Cars): void{
+    this.selectedCarItem = car
+  }
   
+
 
 
 }
--- Sam_Mustapha_Learning_Angular_/src/app/services/car.service.ts
+++ Sam_Mustapha_Learning_Angular_/src/app/services/car.service.ts
@@ -8,12 +8,43 @@ import { cars } from '../Shared/Model/mockCar';
   providedIn: 'root'
 })
 export class CarService {
+  private car: Cars[] = cars;
 
   constructor() { }
+
+
   getCars(): Observable<Cars[]>{
     return of (cars)
 
   }
+
+  addCar(newCar:Cars): Observable<Cars[]>{
+    this.car.push(newCar)
+    return of (this.car);
+  }
+
+  updateCar(updateCar: Cars): Observable<Cars[]>{
+    const index = this.car.findIndex(cars => cars.id === updateCar.id);
+    if(index !== -1){
+      this.car[index] = updateCar;
+    }
+
+
+    return of (this.car);
+  }
+
+  deleteCar(carsId: number):  Observable<Cars[]>{
+    this.car = this.car.filter(cars => cars.id !== carsId)
+    return of (this.car)
+  }
+
+  getCarById(carsId: number):  Observable<Cars[]>{
+    const car = this.car.find(cars => cars.id === carsId)
+    return of ();
+  }
+
+  
+  
     
   
 }

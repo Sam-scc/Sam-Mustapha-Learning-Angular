5820e3ceda195ee6d883171a4c3280a5a6993a88
Sam Mustapha <W0843850@myscc.ca>
2024-10-08 19:31:12 -0400

    router and list

--- Sam_Mustapha_Learning_Angular_/src/main.ts
+++ Sam_Mustapha_Learning_Angular_/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+//import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { CarListComponent } from './app/car-list/car-list.component';
+import { CarListItemComponent } from './app/car-list-item/car-list-item.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes=[
+  {path: 'cars', component: CarListComponent},
+  {path: 'cars/:id', component: CarListItemComponent}
+ ];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+ });
+

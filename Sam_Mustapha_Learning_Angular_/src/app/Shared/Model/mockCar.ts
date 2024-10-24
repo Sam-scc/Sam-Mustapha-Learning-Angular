0201ce5b58b96c353d93167c6be9236cdb26e084
Sam Mustapha <W0843850@myscc.ca>
2024-10-24 15:55:53 -0400

    Finalized

--- Sam_Mustapha_Learning_Angular_/src/app/Shared/Model/mockCar.ts
+++ Sam_Mustapha_Learning_Angular_/src/app/Shared/Model/mockCar.ts
@@ -2,12 +2,12 @@ import { Cars } from "./cars";
 
 export const cars: Cars[]=[
     
-      {id: 1 , carName:"Audi", price :12660, model:"A4 Premium", isAvailable: true,image:"assets/images/audi.png"},
-      {id: 2 , carName:"Ford", price :26000, model:"Mustang", isAvailable: false,image:"assets/images/ford.png"},
-      {id: 3 , carName:"Toyota", price :24000, model:"Camry", isAvailable: true,image:"assets/images/toyota.jpeg"},
-      {id: 4 , carName:"BMW", price :30000, model:"3 Series", isAvailable: true,image:"assets/images/bmw.jpeg"},
-      {id: 5 , carName:"Mercedes-Benz", price :35000, model:"C-Class", isAvailable: true,image:"assets/images/brnx.jpeg"},
-      {id: 6 , carName:"Kia", price :35000, model:"Optima", isAvailable: true,image:"assets/images/kia.jpeg"}
+      {id: 1 , carName:"Audi", price :12660, model:"A4 Premium", isAvailable: true,image:"audi.png"},
+      {id: 2 , carName:"Ford", price :26000, model:"Mustang", isAvailable: false,image:"ford.png"},
+      {id: 3 , carName:"Toyota", price :24000, model:"Camry", isAvailable: true,image:"toyota.jpeg"},
+      {id: 4 , carName:"BMW", price :30000, model:"3 Series", isAvailable: true,image:"bmw.jpeg"},
+      {id: 5 , carName:"Mercedes-Benz", price :35000, model:"C-Class", isAvailable: true,image:"brnx.jpeg"},
+      {id: 6 , carName:"Kia", price :35000, model:"Optima", isAvailable: true,image:"kia.jpeg"}
       
   
     ];
